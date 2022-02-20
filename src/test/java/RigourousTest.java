e241e06cb7f3a4d1110a3038d7898790df12cd72
Cedric <cedric.lphn@yahoo.com>
2022-02-20 17:40:22 +0100

    feat: junit integration

---
+++ src/test/java/RigourousTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class RigourousTest {
+    @Test
+    public void TrueTest() {
+        assertTrue(true);
+    }
+}
